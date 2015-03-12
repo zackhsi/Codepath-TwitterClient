31384af48b9e23bed65d62012e8dcc6664b8a5e2
Zack Hsi <zhsi@lyft.com>
2015-03-11 21:08:46 -0700

    Move layout and logic to new fragment

--- app/src/main/java/com/codepath/apps/twitterclient/activities/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/twitterclient/activities/TimelineActivity.java
@@ -8,35 +8,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AbsListView;
-import android.widget.ListView;
 
 import com.codepath.apps.twitterclient.R;
 import com.codepath.apps.twitterclient.TwitterApplication;
 import com.codepath.apps.twitterclient.TwitterClient;
-import com.codepath.apps.twitterclient.adapters.TweetsArrayAdapter;
-import com.codepath.apps.twitterclient.helpers.EndlessScrollListener;
 import com.codepath.apps.twitterclient.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
-import com.melnykov.fab.FloatingActionButton;
-import com.yalantis.pulltorefresh.library.PullToRefreshView;
 
 import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.util.ArrayList;
+import com.codepath.apps.twitterclient.fragments.TweetsListFragment;
 
 public class TimelineActivity extends ActionBarActivity {
-
-    private TwitterClient client;
-    private ArrayList<Tweet> tweets;
-    private TweetsArrayAdapter aTweets;
-    private ListView lvTweets;
-    private FloatingActionButton fab;
-    private int lastKnownFirst;
-    private PullToRefreshView pullToRefreshView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,102 +29,16 @@ public class TimelineActivity extends ActionBarActivity {
         setContentView(R.layout.activity_timeline);
 
         setupViews();
-
-        client = TwitterApplication.getRestClient();
-        populateTimeline();
     }
 
     private void setupViews() {
-        this.tweets = new ArrayList<>(Tweet.getAll());
-        aTweets = new TweetsArrayAdapter(this, this.tweets);
-
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#469AEA")));
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_twitter);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setTitle("");
-
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(TimelineActivity.this, ComposeActivity.class);
-                startActivityForResult(i, ComposeActivity.COMPOSE_REQUEST_CODE);
-            }
-        });
-
-        lvTweets = (ListView) findViewById(R.id.lvTweets);
-        lvTweets.setAdapter(aTweets);
-
-        lvTweets.setOnScrollListener(new EndlessScrollListener() {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount) {
-                populateTimeline();
-            }
-
-            // https://github.com/makovkastar/FloatingActionButton/issues/99
-            @Override
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                super.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
-                if (firstVisibleItem > lastKnownFirst) {
-                    fab.hide(true);
-                } else if (firstVisibleItem < lastKnownFirst) {
-                    fab.show(true);
-                }
-                lastKnownFirst = firstVisibleItem;
-            }
-
-        });
-
-        pullToRefreshView = (PullToRefreshView) findViewById(R.id.pull_to_refresh);
-        pullToRefreshView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                aTweets.clear();
-                populateTimeline();
-                pullToRefreshView.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        pullToRefreshView.setRefreshing(false);
-                    }
-                }, 1);
-            }
-        });
     }
 
-    private void populateTimeline() {
-        client.getHomeTimeline(Tweet.getMaxId(), new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                Log.d("DEBUG", response.toString());
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
-                        tweet.getUser().save();
-                        tweet.save();
-                        aTweets.add(tweet);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
-                Log.d("DEBUG", errorResponse.toString());
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == ComposeActivity.COMPOSE_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                Tweet tweet = (Tweet) data.getSerializableExtra("tweet");
-                aTweets.insert(tweet, 0);
-            }
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/codepath/apps/twitterclient/fragments/HomeTimelineFragment.java
@@ -0,0 +1,51 @@
+package com.codepath.apps.twitterclient.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.apps.twitterclient.TwitterApplication;
+import com.codepath.apps.twitterclient.TwitterClient;
+import com.codepath.apps.twitterclient.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Created by zackhsi on 3/11/15.
+ */
+public class HomeTimelineFragment extends TweetsListFragment {
+    private TwitterClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        client = TwitterApplication.getRestClient();
+        populateTimeline();
+    }
+
+    protected void populateTimeline() {
+        client.getHomeTimeline(Tweet.getMaxId(), new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Log.d("DEBUG", response.toString());
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweet.getUser().save();
+                        tweet.save();
+                        add(tweet);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.d("DEBUG", errorResponse.toString());
+            }
+        });
+    }
+}
