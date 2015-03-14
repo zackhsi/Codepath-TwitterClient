139f0c4df40606869486630ed33791841d6fa6f6
Zack Hsi <zhsi@lyft.com>
2015-03-13 17:47:07 -0700

    Set layout and fill in info for user header

--- app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ProfileActivity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.twitterclient.R;
 import com.codepath.apps.twitterclient.fragments.UserTimelineFragment;
 import com.codepath.apps.twitterclient.models.User;
+import com.squareup.picasso.Picasso;
 
 
 public class ProfileActivity extends ActionBarActivity {
     User user;
+
+    TextView tvName;
+    TextView tvTagline;
+    TextView tvFollowers;
+    TextView tvFollowing;
+    ImageView ivProfileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +29,11 @@ public class ProfileActivity extends ActionBarActivity {
         setContentView(R.layout.activity_profile);
 
         user = (User) getIntent().getSerializableExtra("user");
+
+        findViews();
+
         getSupportActionBar().setTitle("@" + user.getScreenName());
+        populateProfileHeader(user);
 
         if (savedInstanceState == null) {
             UserTimelineFragment fragmentUserTimeline = UserTimelineFragment.newInstance(user.getScreenName());
@@ -31,6 +44,21 @@ public class ProfileActivity extends ActionBarActivity {
         }
     }
 
+    private void findViews() {
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvTagline = (TextView) findViewById(R.id.tvTagline);
+        tvFollowers = (TextView) findViewById(R.id.tvFollowers);
+        tvFollowing = (TextView) findViewById(R.id.tvFollowing);
+        ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
+    }
+
+    private void populateProfileHeader(User user) {
+        tvName.setText(user.getName());
+        tvTagline.setText(user.getTagline());
+        tvFollowers.setText(user.getFollowersCount() + " Followers");
+        tvFollowing.setText(user.getFriendsCount() + " Following");
+        Picasso.with(this).load(user.getProfileImageURL()).into(ivProfileImage);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/codepath/apps/twitterclient/models/User.java
+++ app/src/main/java/com/codepath/apps/twitterclient/models/User.java
@@ -22,6 +22,15 @@ public class User extends Model implements Serializable {
     @Column(name = "profileImageURL")
     private String profileImageURL;
 
+    @Column(name = "tagline")
+    private String tagline;
+
+    @Column(name = "followersCount")
+    private int followersCount;
+
+    @Column(name = "friendsCount")
+    private int friendsCount;
+
     public User(){
         super();
     }
@@ -38,12 +47,27 @@ public class User extends Model implements Serializable {
         return profileImageURL;
     }
 
+    public String getTagline() {
+        return tagline;
+    }
+
+    public int getFollowersCount() {
+        return followersCount;
+    }
+
+    public int getFriendsCount() {
+        return friendsCount;
+    }
+
     public static User fromJSON(JSONObject json) {
         User user = new User();
         try {
             user.name = json.getString("name");
             user.profileImageURL = json.getString("profile_image_url");
             user.screenName = json.getString("screen_name");
+            user.tagline = json.getString("description");
+            user.followersCount = json.getInt("followers_count");
+            user.friendsCount = json.getInt("friends_count");
         } catch (JSONException e) {
             e.printStackTrace();
         }
